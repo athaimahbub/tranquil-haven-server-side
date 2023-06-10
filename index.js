d10b291d2457b0d8ace03b7cfc99d23e06a3b96b
athaimahbub <athaimahbub2016@gmail.com>
2023-06-10 22:37:49 +0600

    admin role process added

--- index.js
+++ index.js
@@ -53,17 +53,20 @@ async function run() {
       res.send(result);
     })
 
-    app.patch('users/admin/:id', async(req,res) => {
+  
+    app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)};
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          role: "admin"
+          role: 'admin'
         },
       };
 
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
+
     })
 
     // instructor related API
