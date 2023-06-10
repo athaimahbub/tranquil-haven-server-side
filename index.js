2fe3721283ff31e749c41ac4d669a1938051ae03
athaimahbub <athaimahbub2016@gmail.com>
2023-06-10 08:58:11 +0600

    cart data store and update process added

--- index.js
+++ index.js
@@ -44,6 +44,15 @@ async function run() {
     })
 
     // Cart collection process
+    app.get('/carts',async(req,res) => {
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
 
     app.post('/carts', async(req, res) =>{
         const course = req.body;
