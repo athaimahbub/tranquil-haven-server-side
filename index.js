d6cead8207d37fc97fdd17b7edf26db1a2ab0f70
athaimahbub <athaimahbub2016@gmail.com>
2023-06-11 14:01:07 +0600

    Admin working process running

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 // MongoDb 
@@ -51,9 +67,26 @@ async function run() {
       next();
     }
 
+    // AdminInstructor role......................
+    const verifyInstructor = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'instructor') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    };
+  // .................................................
 
     // users related API
     app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // New Added
+    app.get('/users', verifyJWT, verifyInstructor, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -107,20 +140,74 @@ async function run() {
 
     })
 
-    // instructor related API
+    // Admin instructor related API..........................
+    // app.get('/users' , async(req,res) => {
+    //   const result = await instructorCollection.find().toArray();
+    //   res.send(result);
+    // })............No Need
+// ..........................................
+// New added (verifyInstructor parameter remove)
+    app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+    
+      if (req.decoded.email !== email) {
+        res.send({ isAdminInstructor: false });
+      }
+    
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { isAdminInstructor: user?.role === 'instructor' };
+      res.send(result);
+    });
+// ...........................................
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+    // app.get('/carts', verifyJWT, verifyInstructor, async(req,res) => {
+    //   // Remove the cart fetching logic or leave it empty
+    //   res.send([]);
+    // });
+
+    // .................................................................
+    // instructor page related API
     app.get('/instructors' , async(req,res) => {
       const result = await instructorCollection.find().toArray();
       res.send(result);
     })
 
-    // class related API
+    // class related API=================
     app.get('/classes' , async(req,res) => {
       const result = await classesCollection.find().toArray();
       res.send(result);
     })
 
+    app.post('/classes',verifyJWT,verifyAdmin, async(req,res) =>{
+      const newItem = req.body;
+      const result = await classesCollection.insertOne(newItem)
+      res.send(result);
+    } )
+
+    app.delete('/classes/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await classesCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // Cart collection process
-    app.get('/carts', verifyJWT, async(req,res) => {
+    app.get('/carts', verifyJWT,  async(req,res) => {
       const email = req.query.email;
       if(!email){
         res.send([]);
@@ -128,9 +215,9 @@ async function run() {
 
       const decodedEmail = req.decoded.email;
       if (email !== decodedEmail) {
-        return res.status(403).send({ error: true, message: 'porviden access' })
+        return res.status(403).send({ error: true, message: 'forbidden access' })
       }
-      
+
       const query = {email: email};
       const result = await cartCollection.find(query).toArray();
       res.send(result);
